af2139c40e0ebb8218fdce58166fcdda025523f2
Roxana Oprea <oprearoxanaadolfina@gmail.com>
2019-10-08 12:00:09 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+        <h2>Meme Generator</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
